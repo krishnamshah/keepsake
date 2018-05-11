0d64510be279373d5835e930f2c31d6b21d18a14
Krishna Shah <erkrishnashah@outlook.com>
2018-05-11 14:27:22 +0545

    Added laracrust

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Laratrust\LaratrustServiceProvider::class,
 
 
         /*
@@ -213,7 +214,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
 
     ],
 
